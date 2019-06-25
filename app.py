ba8fb2c26a18ba30ac9e613d30e29543231ef01e
Mario <krajacic.mario@gmail.com>
2019-06-25 08:35:25 +0200

    Adding some AJAX and css

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    title = 'Moj site'
+    return render_template('test.html', title=title)
 
+
+@app.route('/data')
+def data():
+    my_data = {
+        'profile': 'Mario',
+        'badges': ['one', 'two', 'tree'],
+    }
+
+    return jsonify(my_data)
 
 if __name__ == '__main__':
     app.run()
